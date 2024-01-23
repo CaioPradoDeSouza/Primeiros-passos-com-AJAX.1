dbf80a718a2072e8712c4b1960fa029e77ee1fbb
Caio Prado de Souza <cpssouza7@gmail.com>
2024-01-22 23:10:05 -0300

    atualizado

---
+++ script.js
@@ -0,0 +1,20 @@
+let botao = document.querySelector('#btn');
+
+let list = document.querySelector('#list');
+
+botao.addEventListener("click", function(){
+    fetch('https://reqres.in/api/users?page=2')
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(response){
+        response.data.forEach(function(user){
+            let item = document.createElement("li");
+            item.classList.add('item');
+
+            item.innerHTML = '<img src= "'+user.avatar+'"/><span>'+user.first_name+'<span>';
+
+            list.appendChild(item);
+        })
+    })
+})
